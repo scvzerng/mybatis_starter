0655713614169bddf015ac41d88deaea85882641
scvzerng <443683059a>
2017-12-18 17:16:43 +0800

    添加注释

--- src/main/java/com/yazuo/intelligent/mybatis/mapper/condition/EntityCondition.java
+++ src/main/java/com/yazuo/intelligent/mybatis/mapper/condition/EntityCondition.java
@@ -26,18 +26,27 @@ public class EntityCondition {
     @Getter
     @Setter
     private Collection<EntityColumn> columns;
+    /**
+     * 查询条件 单个条件
+     */
     @Getter
-    //查询条件 单个条件
     private List<Condition> conditions = new ArrayList<>(1);
+    /**
+     * 查询条件 组条件
+     */
     @Getter
-    //查询条件 组条件
     private List<ConditionGroup> conditionGroups = new ArrayList<>(1);
+
+    /**
+     * 默认链接条件符
+     */
     @Getter
     @Setter
-    //默认链接条件符
     private Logic logic = Logic.AND;
+    /**
+     * 排序条件
+     */
     @Getter
-    //增加排序条件
     private List<OrderBy> orders = new ArrayList<>(1);
 
     private EntityCondition(Class<?> clazz) {
@@ -45,10 +54,19 @@ public class EntityCondition {
         columns = propertyMap.values();
     }
 
+    /**
+     * 初始化实体
+     * @param clazz
+     * @return
+     */
     public static EntityCondition forClass(Class<?> clazz) {
         return new EntityCondition(clazz);
     }
 
+    /**
+     * 设置条件  建议配合前端工具类
+     * @param conditions
+     */
     public void setConditions(List<Condition> conditions) {
         this.conditions = conditions.stream()
                 .filter(checkField())
@@ -56,6 +74,10 @@ public class EntityCondition {
                 .collect(toList());
     }
 
+    /**
+     * 设置组条件 建议配合前端工具类
+     * @param conditionGroups
+     */
     public void setConditionGroups(List<ConditionGroup> conditionGroups) {
         this.conditionGroups = conditionGroups.stream().peek(group -> {
             if (group.getLogic() == null) group.setLogic(logic);
@@ -64,6 +86,10 @@ public class EntityCondition {
         }).collect(toList());
     }
 
+    /**
+     * 设置排序 建议配合前端工具类
+     * @param orderByConditions
+     */
     public void setOrderByConditions(List<OrderBy> orderByConditions) {
         this.orders = orderByConditions.stream()
                 .filter(orderByCondition -> propertyMap.containsKey(orderByCondition.getField()))
@@ -71,26 +97,43 @@ public class EntityCondition {
                 .collect(toList());
     }
 
-    public void filter(String... field){
-       List<String> filterFields = Arrays.asList(field);
+    /**
+     * 过滤指定字段
+     * @param fields 需要过滤的字段
+     */
+    public void filter(String... fields){
+       List<String> filterFields = Arrays.asList(fields);
       this.columns = propertyMap.values()
               .stream()
               .filter(entityColumn -> !filterFields.contains(entityColumn.getProperty()))
               .collect(toList());
     }
-    public void include(String... field){
-        List<String> filterFields = Arrays.asList(field);
+
+    /**
+     * 仅保留指定字段
+     * @param fields 需要保留的字段
+     */
+    public void include(String... fields){
+        List<String> filterFields = Arrays.asList(fields);
         this.columns = propertyMap.values()
                 .stream()
                 .filter(entityColumn -> filterFields.contains(entityColumn.getProperty()))
                 .collect(toList());
     }
 
-
+    /**
+     * 检查字段是否合法
+     * @return
+     */
     private Predicate<Condition> checkField() {
         return condition -> propertyMap.containsKey(condition.getField());
     }
 
+    /**
+     * 检查和初始化condition
+     * @param logic
+     * @return
+     */
     private Consumer<Condition> checkCondition(Logic logic) {
         return condition -> {
             if (condition.getLogic() == null) condition.setLogic(logic);
@@ -98,14 +141,31 @@ public class EntityCondition {
         };
     }
 
+    /**
+     * 添加条件
+     * @param logic 逻辑连接符
+     * @param field 属性名称
+     * @param operator 操作类型
+     * @param value 值
+     */
     public void addCondition(Logic logic, String field, Operator operator, Object value) {
         this.conditions.add(Condition.builder().field(field).logic(logic).column(propertyMap.get(field)).operator(operator).value(value).build());
     }
 
+    /**
+     * 添加 AND field = value 条件
+     * @param field 属性名称
+     * @param value 属性值
+     */
     public void addCondition(String field, Object value) {
         this.addCondition(Logic.AND, field, Operator.EQUALS, value);
     }
 
+    /**
+     * 添加条件组
+     * @param logic 逻辑连接符
+     * @param conditions 一组条件
+     */
     public void addConditionGroup(Logic logic, List<Condition> conditions) {
         conditions.stream()
                 .filter(condition -> condition.getColumn() == null)
@@ -113,10 +173,18 @@ public class EntityCondition {
         this.conditionGroups.add(ConditionGroup.builder().conditions(conditions).logic(logic).build());
     }
 
+    /**
+     * 添加AND连接符条件组
+     * @param conditions 一组条件
+     */
     public void addConditionGroup(List<Condition> conditions) {
         this.addConditionGroup(Logic.AND, conditions);
     }
 
+    /**
+     * 添加排序
+     * @param order 排序
+     */
     public void addOrder(OrderBy order) {
         order.setEntityColumn(propertyMap.get(order.getField()));
         this.orders.add(order);
